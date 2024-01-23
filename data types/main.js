d5f5d703aa1bbae12bd3e425090b3a9c0b32408e
FICHU3000 <sharif.odil@gmail.com>
2024-01-23 22:26:00 +0500

    mini project

--- data types/main.js
+++ data types/main.js
@@ -1,7 +1,35 @@
-let name = "Sharif";
+// let name = "Sharif";
 
-alert(`hello ${1}`);
+// alert(`hello ${1}`);
 
-alert(`hello ${"name"}`);
+// alert(`hello ${"name"}`);
 
-alert(`hello ${name}`);
+// alert(`hello ${name}`);
+
+// typeof undefined // "undefined"
+
+// typeof 0 // "number"
+
+// typeof 10n // "bigint"
+
+// typeof true // "boolean"
+
+// typeof "foo" // "string"
+
+// typeof Symbol("id") // "symbol"
+
+// typeof Math // "object"  (1)
+
+// typeof null // "object"  (2)
+
+// typeof alert // "function"  (3)
+
+let name = prompt('Как вас зовут?');
+
+alert(`Привет, ${name}`)
+
+let age = prompt('Сколько вам лет?')
+
+let age1 = prompt('Сколько лет вашему другу?')
+
+alert(`Ваша разница в возрасте составляет: ${age - age1} лет`)
