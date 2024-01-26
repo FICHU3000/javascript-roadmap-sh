fe6d4694843beda552da16e7a9fcc517e401918d
FICHU3000 <sharif.odil@gmail.com>
2024-01-26 18:21:05 +0500

    for practice

---
+++ Type conversion/main.js
@@ -0,0 +1,31 @@
+// let value = true;
+// alert(typeof value)
+
+// value = String(value)
+// alert(typeof value)
+
+// alert("6" / "2")
+
+// let str = "123";
+// alert(typeof str);
+
+// let num = Number(str);
+
+// alert(typeof num);
+
+// let word = prompt("Введите число")
+
+// let word1 = prompt("Введите второе число")
+
+// alert(`Разделённое значение: ${word % word1}`)
+
+// let apple = "2";
+// let orange = "3";
+
+// alert(apple + orange)
+
+for (let i = 0; i < 5; ++i) {
+  alert(i)
+  let array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+  alert(array[7] % 3)
+}
