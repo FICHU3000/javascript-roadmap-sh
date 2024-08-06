859f528294b4cbd34882658eb4ac572a92139708
FICHU3000 <sharif.odil@gmail.com>
2024-08-06 20:20:58 +0500

    Equality comparisons practice (guess game)

---
+++ guess number/main.js
@@ -0,0 +1,20 @@
+// create secretNumber
+var secretNumber = 4;
+
+// ask user for guess
+var guess = Number(prompt("Guess a number"));
+
+// check if guess is right
+
+if (guess === secretNumber) {
+  alert("YOU GOT IT RIGHT!")
+}
+// otherwise, check if highter
+
+else if (guess > secretNumber)
+  alert("Too high.  Guess again!")
+
+// otherwise, check if lower
+else {
+  alert("Too low.  Guess again!")
+}
