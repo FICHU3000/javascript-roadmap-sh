8653239e5602475944a5561b2cfb24aa64578d0e
FICHU3000 <sharif.odil@gmail.com>
2024-08-08 22:26:38 +0500

    while

---
+++ while/main.js
@@ -0,0 +1,24 @@
+//syntax
+// do
+//   statement
+// while (condition);
+
+
+// let result = '';
+// let i = 0;
+
+// do {
+//   i = i + 1;
+//   result = result + i;
+// } while (i < 5);
+
+// console.log(result);
+
+let result = "";
+let i = 0;
+do {
+  i += 1;
+  result += `${i} `;
+} while (i > 0 && i < 5);
+
+console.log(result);
